606800b8758fae846746f23d57d63659460bbdda
ferer00 <ferer004829@gmail.com>
2025-11-11 11:05:54 +0200

    Added FeatureToggleService.java

---
+++ src/main/java/com/example/cats/featuretoggle/FeatureToggleService.java
@@ -0,0 +1,29 @@
+package com.example.cats.featuretoggle;
+
+
+import com.example.cats.controller.config.FeatureToggleProperties;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class FeatureToggleService {
+
+    private final ConcurrentHashMap<String, Boolean> featureToggles;
+
+    public FeatureToggleService(FeatureToggleProperties featureToggleProperties) {
+        featureToggles = new ConcurrentHashMap<>(featureToggleProperties.getToggles());
+    }
+
+    public boolean check(String featureName) {
+        return featureToggles.getOrDefault(featureName, false);
+    }
+
+    public void enable(String featureName) {
+        featureToggles.put(featureName, true);
+    }
+
+    public void disable(String featureName) {
+        featureToggles.put(featureName, false);
+    }
+}

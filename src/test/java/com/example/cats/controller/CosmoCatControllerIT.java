77651091c8620bd3d83bc4fb103677c57d13e574
ferer00 <ferer004829@gmail.com>
2025-12-10 16:12:07 +0200

    changed CosmoCatControllerIT.java

--- src/test/java/com/example/cats/controller/CosmoCatControllerIT.java
+++ src/test/java/com/example/cats/controller/CosmoCatControllerIT.java
@@ -13,6 +13,7 @@ import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfigurati
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -31,6 +32,7 @@ class CosmoCatControllerIT {
     private MockMvc mockMvc;
 
     @Test
+    @WithMockUser
     @EnabledFeatureToggle(FeatureToggles.COSMO_CATS)
     void getCats_ShouldReturnCats_WhenFeatureIsEnabled() throws Exception {
         mockMvc.perform(get("/api/cosmo-cats"))
@@ -40,6 +42,7 @@ class CosmoCatControllerIT {
     }
 
     @Test
+    @WithMockUser
     @DisabledFeatureToggle(FeatureToggles.KITTY_PRODUCTS)
     void getSpecialProducts_ShouldReturn501_WhenFeatureIsDisabled() throws Exception {
         mockMvc.perform(get("/api/cosmo-cats/special-products"))
